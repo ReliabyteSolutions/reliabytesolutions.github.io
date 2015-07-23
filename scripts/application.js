f101b8ac2484b817af1003aaaefe3839fb01ef42
Jason Banister <jbanister@outlook.com>
2015-07-23 00:38:08 -0500

    Commit

--- scripts/application.js
+++ scripts/application.js
@@ -220,31 +220,6 @@
         this.navigationStatus = navigationSvc.status;
     }
 })();
-(function() {
-    'use strict';
-
-    angular.module('shell')
-        .controller('NotificationPanelCtrl', NotificationPanel);
-
-    function NotificationPanel() {
-
-    }
-})();
-(function() {
-    'use strict';
-
-    angular.module('shell')
-        .controller('ProfilePanelCtrl', ProfilePanel);
-
-    function ProfilePanel() {
-        this.photo = "/assets/user.jpg";
-        this.displayName = "Jason Banister";
-
-        this.view = function() {console.log('view')};
-        this.changePassword = function() {console.log('change-password')};
-        this.signOut = function() {console.log('sign-out')};
-    }
-})();
 
 
 
